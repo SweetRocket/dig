16a7f471d8bc9c4f9b0b50f2a80ffb01ef0c186d
Yeonju <yeonju0420@gmail.com>
2023-03-27 17:51:18 +0900

    yeonju weather

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('dig_site/', include('dig_site.urls')),
+    path('', include('weatherapp.urls')),
 ]
