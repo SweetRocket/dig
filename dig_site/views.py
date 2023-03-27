6e5fc2edfe5cb7a5d9a0915d3ad068ce084f755f
Yeonju <yeonju0420@gmail.com>
2023-03-27 12:08:13 +0900

    yj first commit

--- dig_site/views.py
+++ dig_site/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def ex_info(request):
+    return render(request, 'dig_site/ex_info_new.html')
+
+def index(request):
+    return render(request, 'dig_site/index.html')
