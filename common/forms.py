2ea42a0594e9634682c8f9025e8d77e8fa8b3163
yeonju55 <124329134+yeonju55@users.noreply.github.com>
2023-03-30 18:49:31 +0900

    login and signup (#20)
    Co-authored-by: WonJung Kim <git@won-jung.kim>

--- common/forms.py
+++ common/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserForm(UserCreationForm):
+class SignupForm(UserCreationForm):
     emp_id = forms.CharField(label="사원번호")
     name = forms.CharField(label='이름')
 
--- common/views.py
+++ common/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.http.response import HttpResponseForbidden
 from django.contrib import auth
+from django.views import generic
+from django.urls import reverse
+
+from common.forms import SignupForm
+from common.models import Employee
 
 # Create your views here.
-def login(request):
-    if(request.method =="POST"):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('http://127.0.0.1:8000/')
-        else:
-            return render(request, 'common/Login.html', {'error':'username or password is incorrect'})
-    else:
-        return render(request, 'common/Login.html')
 
-def signup(request):
-    if request.method == "POST":
-        if request.POST['password1'] == request.POST['password2']:
-            user=User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            auth.login(request, user)
-            return redirect('main')
-    return render(request, 'common/signup.html')
+class SignupView(generic.CreateView):
+    form_class = SignupForm
+    template_name = 'common/signup.html'
 
-def logout(request):
-    if request.user.is_authenticated:
-        auth.logout(request)
-    return redirect('main')
+    def form_valid(self, form):
+        # 사원 id 검증
+        emp_id = form.cleaned_data['emp_id']
+        try:
+            emp = Employee.objects.get(emp_id=emp_id)
+        except Employee.DoesNotExist:
+            emp = None
+
+        # 사원이 없거나, 가입이 불가능한 경우
+        if emp is None or emp.joinable is not True:
+            return self.render_to_response({'error': 'Invalid Employee', 'form': form })
+
+        # form 저장
+        form.save()
+
+        # 자동 로그인
+        username = form.cleaned_data['username']
+        raw_password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+
+        return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        return reverse('dig_site:index')
