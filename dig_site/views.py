1d4fdd915a3e7cca4736949763ae15ff2c6a2351
WonJung Kim <git@won-jung.kim>
2023-03-30 20:52:27 +0900

    [WIP] report

--- dig_site/views.py
+++ dig_site/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib import auth
 
-from .models import SiteInfo
+from .models import SiteInfo, Report
 from django.contrib.auth.models import User
+from django.db.models import Q
 # Create your views here.
 
 
@@ -17,7 +17,22 @@ def weather(request):
     return render(request, 'dig_site/weather.html')
 
 def report(request):
-    return render(request, 'dig_site/report.html')
+    q = Q()
+    
+    if (a := request.GET.get('age', None)) is not None:
+        q &= Q(age=a)
+    
+    if (r := request.GET.get('region', None)) is not None:
+        q &= Q(region=r)
+
+    if (s := request.GET.get('query', None)) is not None:
+        q &= Q(title__icontains=s)
+
+    reports = Report.objects.filter(q).all()
+    
+    return render(request, 'dig_site/report.html', {
+        'reports': reports
+    })
 
 def work_daily(request):
     site = SiteInfo.objects.all()
