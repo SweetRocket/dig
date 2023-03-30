535b46af26d1c09a691bd363759533c6d8720d0e
wonjun32 <xkfkfk5454@naver.com>
2023-03-30 15:39:14 +0900

    new

--- dig_site/views.py
+++ dig_site/views.py
@@ -8,3 +8,19 @@ def ex_info(request):
 
 def index(request):
     return render(request, 'dig_site/main.html')
+
+
+def login(request):
+    return render(request, 'dig_site/login.html')
+
+def signup(request):
+    return render(request, 'dig_site/signup.html')
+
+def weather(request):
+    return render(request, 'dig_site/weather.html')
+
+def report(request):
+    return render(request, 'dig_site/report.html')
+
+def work_daily(request):
+    return render(request, 'dig_site/work_daily.html')
