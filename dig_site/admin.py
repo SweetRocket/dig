f2da4e00e02085bdd692e8887e77f062557fb103
WonJung Kim <git@won-jung.kim>
2023-03-31 10:38:12 +0900

    admin

--- dig_site/admin.py
+++ dig_site/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import Report
+from .models import Report, SiteInfo, SiteJoin, WorkHistory
 
 # Register your models here.
 @admin.register(Report)
 class EventAdmin(admin.ModelAdmin):
     fields = ('name', 'age', 'region', 'image', 'url')
+    
+@admin.register(SiteInfo)
+class SiteInfoAdmin(admin.ModelAdmin):
+    fields = ('name', 'address', 'relic_era', 'area')
+    
+@admin.register(SiteJoin)
+class SiteJoinAdmin(admin.ModelAdmin):
+    fields = ('user', 'site')
+
+@admin.register(WorkHistory)
+class WorkHistoryAdmin(admin.ModelAdmin):
+    fields = ('zone', 'note', 'workers', 'images', 'site', 'date')
