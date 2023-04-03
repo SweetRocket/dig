7a9f3c9f2a9af5d0a97b05c5c1be1f6b0c331a06
WonJung Kim <95566897+kimwonj77@users.noreply.github.com>
2023-04-03 15:23:33 +0900

    add comment (#31)
    * comment
    
    * add comment
    
    * comment

--- api/views/weather.py
+++ api/views/weather.py
@@ -1,32 +1,49 @@
 from ..weather_api import request_hourly
-from django.http import HttpResponse, HttpResponseServerError, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 
+# 30분 캐시
 @cache_page(60 * 30)
 def hourly(request):
+    """
+    기상청 API를 이용한 시간별 초단기 예보를 가져오는 API
+    """
+    
+    # 기상청 자체 좌표계 nx, ny 를 사용
     nx = request.GET.get('nx', 60)
     ny = request.GET.get('ny', 125)
 
+    # 기상청 API 를 이용해 데이터를 가져옴
     data = request_hourly(nx, ny)
     
+    # 데이터가 없으면 서버 에러
     if data is None:
-        return HttpResponseServerError('서버 에러')
+        return JsonResponse({ 'status': 'error', 'result': 'no data' }, status=500)
     
+    # 데이터가 있으면 데이터를 가공
     items = data.get('response', {}).get('body', {}).get('items', {}).get('item', [])
     ret = {}
     
+    # 데이터를 가공
     for i in items:
+        # 날짜와 시간을 합쳐서 키로 사용
         date = i['fcstDate'] + 'T' + i['fcstTime']
+        
+        # 날짜 키가 없으면 생성
         if ret.get(date, None) is None:
             ret[date] = {}
 
         try:
+            # 값이 숫자면 float 형으로 변환
             value = float(i['fcstValue'])
         except:
+            # 값이 숫자가 아니면 그대로 사용
             value = i['fcstValue']
         
+        # 카테고리를 키로 사용
         ret[date][i['category']] = value
     
+    # 가공된 데이터를 json 형태로 반환
     resp = JsonResponse({ 'status': 'ok', 'result': ret }, json_dumps_params={'ensure_ascii': False, 'indent': 2})
     # resp["Access-Control-Allow-Origin"] = "*"
     # resp["Access-Control-Allow-Methods"] = "GET, OPTIONS"
--- dig_site/views.py
+++ dig_site/views.py
@@ -19,28 +19,35 @@ def weather(request):
     return render(request, 'dig_site/weather.html')
 
 def report(request):
+    # django 쿼리를 사용하여 데이터를 가져옴
     q = Q()
     
+    # 파라미터에 age가 있으면 age에 해당하는 데이터만 가져옴
     if (a := request.GET.get('age', None)) is not None:
         q &= Q(age=a)
     
+    # 파라미터에 region이 있으면 region에 해당하는 데이터만 가져옴
     if (r := request.GET.get('region', None)) is not None:
         q &= Q(region=r)
 
+    # 파라미터에 query가 있으면 query에 해당하는 데이터만 가져옴
     if (s := request.GET.get('query', None)) is not None:
         q &= Q(name__icontains=s)
 
+    # 가져온 데이터를 쿼리에 맞게 가져옴
     reports = Report.objects.filter(q).all()
     
+    # 가져온 데이터를 사용하여 렌더링
     return render(request, 'dig_site/report.html', {
         'reports': reports
     })
 
-
 @login_required
 def work_daily(request):
+    # 사이트 전체 정보를 가져옴
     site = SiteInfo.objects.all()
     
+    # 페이지에 맞게 데이터를 전달
     data = {
         'sites': [ { 'id': s.pk, 'name': s.name } for s in site ]
     }
