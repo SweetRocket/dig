bdad559f038cf4aafb705c8fcde2cf4eee8f449f
WonJung Kim <git@won-jung.kim>
2023-04-04 09:56:34 +0900

    video equipment

--- common/admin.py
+++ common/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Image, Employee, Profile
+from .models import Image, Employee, Profile, VideoEquipment
 
 # Register your models here.
 @admin.register(Image)
@@ -14,3 +14,7 @@ class EmployeeAdmin(admin.ModelAdmin):
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     fields = ('user', 'emp', 'sex', 'age', 'phone_number', 'address', 'emg_contact')
+    
+@admin.register(VideoEquipment)
+class VideoEquipmentAdmin(admin.ModelAdmin):
+    fields = ('name', 'device_address')
