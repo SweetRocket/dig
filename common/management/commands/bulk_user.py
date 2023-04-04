82959b3aa294e6b622e5d9bf56460aa200e86fb7
WonJung Kim <git@won-jung.kim>
2023-04-04 16:23:39 +0900

    bulk user

---
+++ common/management/commands/bulk_user.py
@@ -0,0 +1,144 @@
+from django.core.management.base import BaseCommand, CommandError
+from common.models import Profile, User, PositionChoices, Employee
+from django.contrib.auth.hashers import make_password
+
+import pathlib
+import pandas as pd
+import numpy as np
+import random
+
+import logging
+
+logger = logging.getLogger("cli")
+
+class Command(BaseCommand):
+    help = "Bulk create reports"
+    
+    # add_argument 를 사용하여 명령어에 사용할 파라미터를 추가
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('file', type=pathlib.Path)
+    
+    # handle 함수를 사용하여 명령어가 실행될 때 실행될 코드를 작성
+    def handle(self, *args, **options):
+        # 파일 경로를 가져옴
+        source = options['file']
+        
+        # 엑셀 파일을 읽어옴
+        if source.suffix in ['.xls', '.xlsx']:
+            df = pd.read_excel(source)
+        # csv 파일을 읽어옴
+        elif source.suffix in ['.csv']:
+            df = pd.read_csv(source)
+        # 지원하지 않는 파일 형식일 경우 에러를 발생시킴
+        else:
+            raise CommandError('Unsupported file type')
+        
+        # 자체 인덱스 설정
+        df.set_index('번호', inplace=True)
+        
+        # 직급 변환
+        conv_position = {
+            name: num for num, name in PositionChoices.choices
+        }
+        
+        for k, d in df.to_dict('index').items():
+            # 이름 분리
+            last_name = d['직원이름'][0]
+            first_name = d['직원이름'][1:]
+
+            # 직급
+            position = d['직급']
+            position = conv_position[position]
+            
+            # FIXME: 데이터 고쳐야함
+            if position == PositionChoices.SUPPORTER or position == PositionChoices.SEMI_RESEARCHER:
+                position = random.randint(4, 6)
+            
+            # 나이
+            age = d['나이']
+            
+            # FIXME: 데이터 고쳐야함
+            if np.isnan(age):
+                age = random.randint(20, 70)
+            
+            # 성별
+            sex = d['성별']
+            
+            # 연락처
+            phone_number = d['연락처']
+            
+            # 이메일
+            address = d['주소']
+            
+            # 비상연락처
+            emg_contact = d['비상연락처']
+            
+            # 아이디
+            username = d['아이디']
+            
+            # 비밀번호
+            password = d['비밀번호']
+            
+            # 직원번호
+            emp = d['직원번호']
+            
+            # 직원번호로 직원 생성
+            e, _ = Employee.objects.get_or_create(
+                emp_id = emp,
+                defaults = {
+                    'joinable': True,
+                }
+            )
+            
+            # 직원 재설정
+            e.joinable = True
+            
+            e.save()
+            
+            # 유저 생성
+            u, _ = User.objects.get_or_create(
+                username = username,
+                defaults= {
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'password': make_password(password),
+                }
+            )
+            
+            # 유저 재설정
+            u.first_name = first_name
+            u.last_name = last_name
+            u.password = make_password(password)
+            
+            u.save()
+            
+            # 프로필 생성          
+            p, _ = Profile.objects.get_or_create(
+                user = u,
+                defaults = {
+                    'user': u,
+                    'emp': e,
+                    'PositionChoices': position,
+                    'age': age,
+                    'phone_number': phone_number,
+                    'address': address,
+                    'emg_contact': emg_contact,
+                    'sex': sex
+                }
+            )
+            
+            # 프로필 재설정
+            p.user = u
+            p.emp = e
+            p.PositionChoices = position
+            p.age = age
+            p.phone_number = phone_number
+            p.address = address
+            p.emg_contact = emg_contact
+            p.sex = sex
+            
+            p.save()
+            
+            logger.info(f'Created user {u}')
+        logger.info('Done')
+        
--- common/models.py
+++ common/models.py
@@ -7,66 +7,74 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class PositionChoices(models.IntegerChoices):
-    ADMIN = 0, 'Admin'
-    MANAGER = 1, 'Manager'
-    EMPLOYEE = 2, 'Employee'
+    ADMIN = 0, '관리자'
+    HEAD = 1, '조사단장'
+    LEAD = 2, '책임조사원'
+    RESEARCHER = 3, '조사원'
+    SEMI_RESEARCHER = 4, '준조사원'
+    SUPPORTER = 5, '보조원'
+    WORKER = 6, '근로자'
+    EMPLOYEE = 7, '직원'
+
+
+# 전체 직원
+class Employee(models.Model):
+    # 직원 번호를 CharField, Primary Key로 설정
+    emp_id = models.CharField(max_length=32, primary_key=True)
+
+    # 가입 가능 여부 필드
+    joinable = models.BooleanField(default=True)
+
 
 # 프로필
 class Profile(models.Model):
     # Django 기본 User 모델과 1:1, Cascade 관계 설정
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     # 직원 모델과의 1:1, Cascade 관계 설정
-    emp = models.OneToOneField("Employee", on_delete=models.CASCADE, null=True, blank=True)
+    emp = models.OneToOneField(
+        Employee, on_delete=models.CASCADE, null=True, blank=True)
 
     # 성별
     sex = models.CharField(max_length=32)
-    
+
     # 직급
-    PositionChoices = models.PositiveIntegerField(choices=PositionChoices.choices, default=PositionChoices.EMPLOYEE)
-    
+    PositionChoices = models.PositiveIntegerField(
+        choices=PositionChoices.choices, default=PositionChoices.EMPLOYEE)
+
     # 나이
     age = models.PositiveIntegerField(default=0)
-    
+
     # 연락처
-    phone_number = PhoneNumberField(region='KR') # type: ignore
-    
+    phone_number = PhoneNumberField(region='KR')  # type: ignore
+
     # 주소
     address = models.TextField(blank=True)
-    
+
     # 비상 연락처
-    emg_contact = PhoneNumberField(region='KR') # type: ignore
+    emg_contact = PhoneNumberField(region='KR')  # type: ignore
 
     def __str__(self):
-        return self.user.username
-
-    
-# 전체 직원
-class Employee(models.Model):
-    # 직원 번호를 CharField, Primary Key로 설정
-    emp_id = models.CharField(max_length=32, primary_key=True)
-    
-    # 가입 가능 여부 필드
-    joinable = models.BooleanField(default=True)
+        return f'{self.user.last_name}{self.user.first_name}, {PositionChoices(self.PositionChoices).label} ({self.user.username})'
 
 
 # 로그
 class Log(models.Model):
     # 발생 유저
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    
+
     # 내용
     content = models.TextField()
-    
+
     # 생성(발생) 시간
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-# d이미지
+# 이미지
 class Image(models.Model):
     # 이미지 파일
     image = models.ImageField(upload_to='images/')
-    
+
     def __str__(self):
         return self.image.name
 
@@ -77,12 +85,12 @@ class VideoEquipment(models.Model):
     name = models.CharField(max_length=32)
     # 영상 기기 주소
     device_address = models.TextField(null=False, blank=False)
-    
+
     def __str__(self):
         return self.name
 
-    
-# 유저 생성시 유저의 Profile도 생성 
+
+# 유저 생성시 유저의 Profile도 생성
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
